d2dce10de80eeb0376c4ab7bc2322217bae355fb
ZhannaKlimanova <zklimanova@yahoo.com>
2020-08-21 17:27:19 -0400

    mergeSort, quickSort added

--- Java_Rhythms/SortingAlgorithms.java
+++ Java_Rhythms/SortingAlgorithms.java
@@ -1,6 +1,113 @@
-package Java_Rhythms;
+package Java_algoRhythms;
+import java.util.ArrayList;
 
 public class SortingAlgorithms {
+	
+	/* O(nlog(n))
+	 * mergeSort is a divide and conquer algorithm: 
+	 * 1. partitions the list into two halves 
+	 * 2. sorts each half recursively 
+	 * 3. merge the sorted half maintaining the order
+	 * 
+	 * Best case: O(nlog(n))
+	 * Worst case: O(nlog(n))
+	 * 
+	 * mergeSort is more efficient and works faster than quickSort
+	 * when dealing with larger data sets. However, mergeSort does take up 
+	 * more space as it uses an extra list, which can hurt the sorting 
+	 * performance for big data sets. 
+	 */
+	
+	public static ArrayList<Integer> mergeSort(ArrayList<Integer> list) {
+		if (list.size() == 1) {
+			return list;
+		}
+		else {
+			int middle = (int) Math.ceil((list.size()-1)/2.0);			
+			ArrayList<Integer> list1 = new ArrayList<Integer>(middle);
+			ArrayList<Integer> list2 = new ArrayList<Integer>(list.size() - middle);
+			
+			for (int i = 0; i < middle; i++) {
+				list1.add(list.get(i));
+			}
+			for (int i = middle; i < list.size(); i++) {
+				list2.add(list.get(i));
+			}
+			
+			list1 = mergeSort(list1);
+			list2 = mergeSort(list2);
+			
+			int listSize = list.size();
+			return merge(listSize, list1, list2);
+		}
+	}
+	
+	public static ArrayList<Integer> merge(int listSize, ArrayList<Integer> list1, ArrayList<Integer> list2) {
+		ArrayList<Integer> sortedList = new ArrayList<Integer>(listSize);
+		
+		while (!list1.isEmpty() && !list2.isEmpty()) {
+			if (list1.get(0) < list2.get(0)) {
+				sortedList.add(list1.remove(0));
+			}
+			else {
+				sortedList.add(list2.remove(0));
+			}
+		}
+		while (!list1.isEmpty()) {
+			sortedList.add(list1.remove(0));
+		}
+		while (!list2.isEmpty()) {
+			sortedList.add(list2.remove(0));
+		}
+		return sortedList;
+	}
+	
+	/* O(nlog(n))
+	 * quickSort is a divide and conquer algorithm: 
+	 * 1. pick an element of the array (the pivot)
+	 * 2. partition the list moving the pivot to its correct position 
+	 * 	  making sure all the lower elements are on its left and all 
+	 *    the larger elements are on its right
+	 * 3. sort the left part and the right part recursively
+	 * 4. keep repeating until there's nothing left to sort
+	 * 
+	 * Best case: O(nlog(n))
+	 * Worst case: O(n^2) when list is already sorted in either ascending or descending order
+	 * 
+	 * quickSort is more efficient and works faster than mergeSort
+	 * when dealing with smaller data sets. 
+	 */
+	
+	public static void quickSort(int[] list, int leftSide, int rightSide) {
+		if (leftSide >= rightSide) { // base case where there is only one element which does not need sorting
+			return;
+		}
+		else {
+			int i = placeAndDivide(list, leftSide, rightSide); // i is index where the pivot is placed
+			quickSort(list, leftSide, i-1); 
+			quickSort(list, i+1, rightSide);
+		}
+	}
+	
+	public static int placeAndDivide(int[] list, int leftSide, int rightSide) {
+		int pivot = list[rightSide]; // pick the rightmost element for the pivot
+		int wall = leftSide-1; // place wall to the left
+		
+		for (int i = leftSide; i < rightSide; i++) { // go through all elements and compare them to the pivot
+			if (list[i] < pivot) {
+				wall++;  // move wall 
+				int temp = list[wall]; // move element behind wall
+				list[wall] = list[i];
+				list[i] = temp;
+			}
+		}
+		
+		int temp = list[wall+1]; // move pivot next to the wall
+		list[wall+1] = list[rightSide];
+		list[rightSide] = temp;
+		
+		return wall+1;
+	}
 	
 	/* O(n^2)
 	 * bubbleSort maintains a sorted list at the end and 
@@ -9,6 +116,7 @@ public class SortingAlgorithms {
 	 * Best case: O(n) when there's no swapping and list is sorted initially
 	 * Worst case: O(n^2) 
 	 */
+	
 	public static void bubbleSort(int[] list) {
 		int counter = 0;
 		boolean swapHappened = false;
--- Java_Rhythms/SortingAlgorithmsTester.java
+++ Java_Rhythms/SortingAlgorithmsTester.java
@@ -1,5 +1,6 @@
-package Java_Rhythms;
+package Java_algoRhythms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SortingAlgorithmsTester {
@@ -8,6 +9,9 @@ public class SortingAlgorithmsTester {
 		int[] unsortedArray = {5,4,3,2,1};
 		int[] sortedArray = {1,2,3,4,5};
 		int[] anotherArray = {1,5,3,4,2};
+		
+		ArrayList<Integer> intArray = new ArrayList<>(Arrays.asList(5,4,3,2,1));
+		ArrayList<Integer> intArray2 = new ArrayList<>(Arrays.asList(1,5,3,4,2));
 		
 		// bubbleSort Tester
 //		SortingAlgorithms.bubbleSort(unsortedArray); // worst case O(n^2)
@@ -30,11 +34,23 @@ public class SortingAlgorithmsTester {
 //		SortingAlgorithms.insertionSort(unsortedArray); // worst case O(n^2)
 //		System.out.println(Arrays.toString(unsortedArray)); // prints {1,2,3,4,5}
 //		
-		SortingAlgorithms.insertionSort(sortedArray); // best case O(n)
-		System.out.println(Arrays.toString(sortedArray)); // prints {1,2,3,4,5}
+//		SortingAlgorithms.insertionSort(sortedArray); // best case O(n)
+//		System.out.println(Arrays.toString(sortedArray)); // prints {1,2,3,4,5}
 //		
 //		SortingAlgorithms.insertionSort(anotherArray); 
 //		System.out.println(Arrays.toString(anotherArray)); // prints {1,2,3,4,5}
+		
+		// mergeSort Tester
+//		System.out.println(SortingAlgorithms.mergeSort(intArray).toString());
+//		System.out.println(SortingAlgorithms.mergeSort(intArray2).toString());
+	
+		// quickSort Tester 
+//		SortingAlgorithms.quickSort(unsortedArray, 0, unsortedArray.length-1);
+//		System.out.println(Arrays.toString(unsortedArray));
+//		
+//		SortingAlgorithms.quickSort(anotherArray, 0, anotherArray.length-1);
+//		System.out.println(Arrays.toString(anotherArray));
+		
 	}
 }
  
